adbfb4d7e5d1668baa535a1f4e43b27338ab33e7
Istvan Marhefka <istvan.marhefka@swisscom.com>
2018-10-20 22:12:21 +0200

    building first form

---
+++ src/main/java/hu/waldorf/finance/FamilyDTO.java
@@ -0,0 +1,25 @@
+package hu.waldorf.finance;
+
+public class FamilyDTO {
+    private String supporter;
+    private String supporterEmail;
+
+    public FamilyDTO() {
+    }
+
+    public String getSupporter() {
+        return supporter;
+    }
+
+    public void setSupporter(String supporter) {
+        this.supporter = supporter;
+    }
+
+    public String getSupporterEmail() {
+        return supporterEmail;
+    }
+
+    public void setSupporterEmail(String supporterEmail) {
+        this.supporterEmail = supporterEmail;
+    }
+}
--- src/main/java/hu/waldorf/finance/MainView.java
+++ src/main/java/hu/waldorf/finance/MainView.java
@@ -3,6 +3,10 @@ package hu.waldorf.finance;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,11 +20,36 @@ public class MainView extends VerticalLayout {
     public MainView(FamilyService familyService) {
         this.familyService = familyService;
 
-        add(new Button("Click me", e -> showNotification()));
+        TextField tamogato = new TextField("Támogató (Szerződő)");
+        TextField emailCim = new TextField("Email-cím");
+
+        Binder<FamilyDTO> binder = new Binder<>();
+        binder
+                .forField(tamogato)
+//                .asRequired()
+                .bind(FamilyDTO::getSupporter, FamilyDTO::setSupporter);
+
+        binder
+                .forField(emailCim)
+                .withValidator(new EmailValidator("Valódi e-mail címet adjon meg."))
+                .bind(FamilyDTO::getSupporterEmail, FamilyDTO::setSupporterEmail);
+
+        add(tamogato,
+                emailCim,
+                new Button("Click me", e -> {
+                    FamilyDTO newFamily = new FamilyDTO();
+                    try {
+                        binder.writeBean(newFamily);
+                    } catch (ValidationException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    showNotification(newFamily);
+                }
+                ));
     }
 
-    private void showNotification() {
-        Notification.show("Hello Spring+Vaadin user!");
+    private void showNotification(FamilyDTO newFamily) {
+        Notification.show("Hello Spring+Vaadin user! " + newFamily.getSupporter() + " - " + newFamily.getSupporterEmail());
         familyService.addFamily();
     }
 }
